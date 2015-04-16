b6154a90c2045b8996a4668ea9ced90d2d1f5227
Huan HOANG <huanhvhd@gmail.com>
2015-04-16 18:40:26 +0700

    Create package

---
+++ NetViet.Patterns/Specification/GroupSpecification.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace NetViet.Patterns.Specification
+{
+    public abstract class GroupSpecification<T> : ISpecification<T>
+    {
+        protected readonly IEnumerable<ISpecification<T>> Specifications;
+
+        protected GroupSpecification(params ISpecification<T>[] specifications)
+        {
+            Specifications = specifications;
+        }
+
+        public abstract bool IsSatisfiedBy(T @object);
+    }
+}
